c7bb5af97fed099f02b2400def9522f39e8849c4
Antonio Mone <antoniomone97@gmail.com>
2025-11-22 00:28:51 +0100

    seeds 0 and 1 for cheetah

--- training_transformers.sh
+++ training_transformers.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 
+for seed in 2 3 4; do
+    python new_main_tr.py -MHC --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data  --set_epochs 200
+done
+
+
 for seed in 0 1 2 3 4; do
-    python new_main_tr.py -MHC --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data --with_lines --set_epochs 200
-    python new_main_tr.py -MHW --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data --with_lines --set_epochs 200
-    python new_main_tr.py -MHo2 --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data --with_lines --set_epochs 200
-    python new_main_tr.py -MHo --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data --with_lines --set_epochs 200
+    python new_main_tr.py -MHW --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data --set_epochs 200
+done
+
+
+for seed in 0 1 2 3 4; do
+    python new_main_tr.py -MHo2 --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data --set_epochs 200
+done
+
+for seed in 0 1 2 3 4; do
+    python new_main_tr.py -MHo --epochs 200 --train  --seed $seed --use_set_encoder --train_set --save_data --set_epochs 200
 done
