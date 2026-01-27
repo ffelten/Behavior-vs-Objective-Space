74353a1bf00ab7bbebdcfdd571d23beac4053364
Antonio Mone <antoniomone97@gmail.com>
2026-01-27 09:19:30 +0100

    added 10 seeds of experiments for all the final models without the decoder (having the model as close as possible to the one from my paper) and for the baseline model

--- training_transformers.sh
+++ training_transformers.sh
@@ -2,20 +2,20 @@
 
 # Transformer
 
-for seed in {0..4}; do
+for seed in {0..9}; do
     python new_main_tr.py -MHC --epochs 100 --train --seed $seed --save_data
 done
 
-for seed in {0..4}; do
+for seed in {0..9}; do
     python new_main_tr.py -MHo --epochs 100 --train --seed $seed --save_data
 done
 
 # Baseline model
 
-for seed in {0..4}; do
+for seed in {0..9}; do
     python new_main_tr.py -MHC --epochs 100 --train --seed $seed --save_data --use_mlp_baseline --model_prefix basic
 done
 
-for seed in {0..4}; do
+for seed in {0..9}; do
     python new_main_tr.py -MHo --epochs 100 --train --seed $seed --save_data --use_mlp_baseline --model_prefix basic
 done
--- training_transformers_dst.sh
+++ training_transformers_dst.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-for seed in {0..4}; do
-    python new_main_tr.py -DSTS --epochs 100 --train --seed $seed --d_hid 32 --save_data --with_lines
-    python new_main_tr.py -DSTLR --epochs 100 --train --seed $seed --d_hid 32 --save_data --with_lines
+for seed in {0..9}; do
+    python new_main_tr.py -DSTS --epochs 100 --train --seed $seed --d_hid 32 --save_data
+    python new_main_tr.py -DSTLR --epochs 100 --train --seed $seed --d_hid 32 --save_data
 done
 
 # Baseline model
 
-for seed in {0..4}; do
+for seed in {0..9}; do
     python new_main_tr.py -DSTS --epochs 100 --train  --seed $seed --save_data  --d_hid 32 --use_mlp_baseline --model_prefix basic
     python new_main_tr.py -DSTLR --epochs 100 --train  --seed $seed --save_data --d_hid 32 --use_mlp_baseline --model_prefix basic
 done
